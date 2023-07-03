4ca8e8bf75e7a6fc9ff947429ec084fc02020b7f
EderGM <127796842+EderGM@users.noreply.github.com>
2023-07-03 15:45:07 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ git init
 git pull https://github.com/rolhuama/pruebasdecodigo.git
 gradle bootjar
 docker build -t aplicacion .
+docker login -u edergm
 docker tag aplicacion edergm/aplicacion
 docker push edergm/aplicacion
