db14a552396648961e065c433b3aedbb54bde17f
EderGM <127796842+EderGM@users.noreply.github.com>
2023-07-19 10:43:36 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,6 @@
 
 mkdir aplicacion
 cd aplicacion
-git init
-git pull https://github.com/rolhuama/pruebasdecodigo.git
 gradle bootjar
 docker build -t aplicacion .
 docker tag aplicacion edergm/aplicacion
