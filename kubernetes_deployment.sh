13e523f63cd16e690fcf24cb640b8c1cc7266914
EderGM <127796842+EderGM@users.noreply.github.com>
2023-07-21 12:37:53 -0500

    Update kubernetes_deployment.sh

--- kubernetes_deployment.sh
+++ kubernetes_deployment.sh
@@ -3,4 +3,4 @@
 kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
 sleep 100
-kubectl port-forward deployment.apps/devops 9000:8080 &
+kubectl port-forward deployment.apps/devops 8500:8080 &
